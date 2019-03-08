4c4eaf38a559d42c8b89e2e757100611c0ae730f
Kristjan Pärnmets <48313701+kparnmets@users.noreply.github.com>
2019-03-08 20:32:16 +0200

    Update Valuuta.java

--- Valuuta.java
+++ Valuuta.java
@@ -3,4 +3,11 @@ package Ryhmatoo;
 public class Valuuta {
     private double väärtusEurodes;
     
+    public double getVäärtusEurodes() { return väärtusEurodes; }
+    
+    public void setVäärtusEurodes(double väärtusEurodes) { this.väärtusEurodes = väärtusEurodes; }
+    
+    public Valuuta(double väärtusEurodes) {
+        this.väärtusEurodes = väärtusEurodes;
+    }
 }
